66bc681f8cd66fd0efca2fac6222502d43049284
Luís Borges de Oliveira <lbo@siscog.pt>
2019-12-08 08:23:49 +0000

    Initial commit

---
+++ qmetaobject.cpp
@@ -0,0 +1,5 @@
+#include "qmetaobject.h"
+
+qmetaobject::qmetaobject()
+{
+}
---
+++ qmetaobject.h
@@ -0,0 +1,12 @@
+#ifndef QMETAOBJECT_H
+#define QMETAOBJECT_H
+
+#include "libqmetaobject_global.h"
+
+class LIBQMETAOBJECT_EXPORT qmetaobject
+{
+public:
+    qmetaobject();
+};
+
+#endif // QMETAOBJECT_H
